41d70e4b99a2b91cb4d6ab73c324264e42055f48
ChillyWillyGuru <ChillyWillyGuru@e68fe1e2-52bc-dc62-6954-b9b5b324de01>
2010-04-22 03:39:55 +0000

    cleanup and bug fixes

--- trunk/MD/NeoMythMenuDX/extra files/libff/optimize/genesis/memory.s
+++ trunk/MD/NeoMythMenuDX/extra files/libff/optimize/genesis/memory.s
@@ -8,52 +8,48 @@
 mem_cmp:
 		movea.l 4(sp),a0
 		movea.l 8(sp),a1
-		move.l 12(sp),d1
-		moveq #1,d0
-
+		move.l  12(sp),d1
+		moveq   #0,d0
 0:
-		cmpi.l #0,d1
-			ble 1f
+		tst.l   d1
+        ble.b   1f
 
-		subq.l #1,d1
-		move.b (a0)+,d0
-		sub.b (a1)+,d0
-
-		cmpi.b #0,d0
-			beq.b 0b
-1:	
+		subq.l  #1,d1
+		move.b  (a0)+,d0
+		sub.b   (a1)+,d0
+        beq.b   0b
+1:
+        ext.w   d0
+        ext.l   d0
 		rts
 
 | int chk_chr (const char* str, int chr)
         .global chk_chr
 chk_chr:
 		movea.l 4(sp),a0
-		move.l 8(sp),d1
-		move.b d1,d0
-
+		move.l  8(sp),d1
+		move.l  d1,d0
 1:
-		cmpi.b #0,(a0)
-			beq.b 2f
+		tst.b   (a0)
+        beq.b   2f
 
-		cmp.b (a0)+,d0
-			bne.b 1b
-
+		cmp.b   (a0)+,d0
+		bne.b   1b
 2:
-		move.b (a0),d0
+		move.b  (a0),d0
 		rts
 
 | void mem_set(void* dst, int val, int cnt)
         .global mem_set
 mem_set:
 		movea.l 4(sp),a0
-		move.l 8(sp),d0
-		move.l 12(sp),d1
-
-		bra.b 2f
+		move.l  8(sp),d0
+		move.l  12(sp),d1
+		bra.b   2f
 1:
-		move.b d0,(a0)+ 
+		move.b  d0,(a0)+
 2:
-		dbra d1,1b
+		dbra    d1,1b
 		rts
 
 | void mem_cpy(void* dst, const void* src, int cnt)
@@ -61,13 +57,10 @@ mem_set:
 mem_cpy:
 		movea.l 4(sp),a0
 		movea.l 8(sp),a1
-		move.l 12(sp),d0
-
-		bra.b 2f
+		move.l  12(sp),d0
+		bra.b   2f
 1:
-		move.b (a1)+,(a0)+
+		move.b  (a1)+,(a0)+
 2:
-		dbra d0,1b
-
+		dbra    d0,1b
 		rts
-
