e82b231d1fc22ad8cf955442101d52821744a240
ChillyWillyGuru <ChillyWillyGuru@e68fe1e2-52bc-dc62-6954-b9b5b324de01>
2010-06-10 16:41:17 +0000

    Added CPLD ID and Flash type to display.

--- trunk/MD/NeoMythMenuDX/crt0.s
+++ trunk/MD/NeoMythMenuDX/crt0.s
@@ -443,14 +443,14 @@ _setup_md_myth:
         lea     0xA10000,a1
         move.w  #0x0000,RST_SEL(a1)
         move.w  #0x00FF,RST_IO(a1)
+
         move.w  #0x00FF,CPLD_ID(a1)
         cmpi.b  #0x63,0x300002
         bne.b   0f
         cmpi.b  #0x63,0x30000A
         bne.b   0f
-        cmpi.b  #0x04,0x300000
-        bne.b   0f
-| V12 hardware
+|        cmpi.b  #0x04,0x300000
+|        bne.b   0f
         move.w  #0x0000,CPLD_ID(a1)
 0:
         move.w  #0x0000,OPTION_IO(a1)   /* set mode 0 */
