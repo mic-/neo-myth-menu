18a3d9647bcd2dc7168b164832a623d8ea5432ac
conleon1988 <conleon1988@e68fe1e2-52bc-dc62-6954-b9b5b324de01>
2010-08-25 06:25:17 +0000

    Huge libio update - Added device extensions and more than 1 device(SD) can be used

--- N64/NeoN64Shell/libs/libio.h
+++ N64/NeoN64Shell/libs/libio.h
@@ -1,58 +1,47 @@
 #ifndef _libio_h_
 #define _libio_h_
 
-//LIBIO 0.2
+//LIBIO 0.3
 
 #include <ff.h>
 #include <stdint.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include "libio_conf.h"
 
-#undef IO_TARGET_PLATFORM_SNES
-#undef IO_TARGET_PLATFORM_MD32X
-#undef SNES_LOVELY_POINTERS
-#define IO_TARGET_PLATFORM_N64
+typedef enum IOD_Type IOD_Type;
+enum IOD_Type
+{
+    IO_DEVICE_SD = 0xff,/*SD*/
+    IO_DEVICE_SRAM,/*On board sram*/
+    IO_DEVICE_EEPROM,/*On board eeprom(Mainly for N64 emulated eeprom)*/
+    IO_DEVICE_GBA_SRAM,/*GBA sram*/
+    IO_DEVICE_MYTH_PSRAM,/*Myth on board PSRAM. WARNING : N64 DOES NOT HAVE ONE*/
+    IO_DEVICE_PSRAM/*GBA "ZIPRAM"*/
+};
 
-#define IO_Handle FIL
+//#define IO_Handle FIL
+typedef struct IO_Handle IO_Handle;
+struct IO_Handle
+{
+    IOD_Type type;
+    union
+    {
+        uint32_t address;
+        FIL handle;
+    };
+};
+
 #define IO_HandleInfo FILINFO
 #define IO_HandleResult FRESULT
 #define IO_DirHandle DIR
-#undef IO_FULL_OPT
-
-/*MD / 32X*/
-#if (IO_TARGET_PLATFORM_MD32X)
-	#define MAX_FILE_HANDLES (16)
-	#define MAX_DIR_HANDLES (8)
-	#define DISABLE_INTERRUPTS asm("move.w 0x2700,sr")
-	#define ENABLE_INTERRUPTS asm("move.w 0x2000,sr")
-#endif
-	
-/*SNES*/
-#if defined(IO_TARGET_PLATFORM_SNES)
-	#define MAX_FILE_HANDLES (8)
-	#define MAX_DIR_HANDLES (4)
-	#define SNES_LOVELY_POINTERS
-	#define DISABLE_INTERRUPTS asm("sei")
-	#define ENABLE_INTERRUPTS asm("cli")
-#endif
-
-/*N64*/
-#if defined(IO_TARGET_PLATFORM_N64)
-	#define DISABLE_INTERRUPTS asm("\tmfc0 $8,$12\n\tla $9,~1\n\tand $8,$9\n\tmtc0 $8,$12\n\tnop":::"$8","$9")
-	#define ENABLE_INTERRUPTS asm("\tmfc0 $8,$12\n\tori $8,1\n\tmtc0 $8,$12\n\tnop":::"$8")
-#endif
-
-#ifndef DISABLE_INTERRUPTS
-	#define DISABLE_INTERRUPTS
-#endif
-
-#ifndef ENABLE_INTERRUPTS
-	#define ENABLE_INTERRUPTS
-#endif
 
 enum
 {
-	IO_SEEK_SET = 0xFFFF,
-	IO_SEEK_END,
-	IO_SEEK_CUR
+    IO_SEEK_SET = 0xFFFF,
+    IO_SEEK_END,
+    IO_SEEK_CUR
 };
 
 void io_init(uint32_t genericBufSize);
@@ -60,7 +49,7 @@ void io_shutdown();
 void io_close(IO_Handle* f);
 void io_remove(const int8_t* filename);
 
-IO_Handle* io_open(const int8_t* filename,const int8_t* mode);
+IO_Handle* io_open(const int8_t* device,const int8_t* filename,const int8_t* mode);
 
 int32_t io_getc(IO_Handle* f);
 int32_t io_eof(IO_Handle* f);
