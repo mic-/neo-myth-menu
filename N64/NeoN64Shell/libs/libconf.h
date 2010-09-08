e19283b73aedfd35cf8041ab8238ecc550197b04
conleon1988 <conleon1988@e68fe1e2-52bc-dc62-6954-b9b5b324de01>
2010-09-08 07:59:59 +0000

    SD burn speed improvements , integrated libconfig (not used yet)

--- N64/NeoN64Shell/libs/libconf.h
+++ N64/NeoN64Shell/libs/libconf.h
@@ -22,19 +22,23 @@ struct ConfigEntry
 
 void config_init();
 void config_shutdown();
-void config_remove(const int8_t* variable);
-
 ConfigEntry* config_push(const int8_t* variable,const int8_t* value);
+ConfigEntry* config_pushI(const int8_t* variable,const int32_t value);
+ConfigEntry* config_pushF(const int8_t* variable,const float value);
+ConfigEntry* config_pushD(const int8_t* variable,const double value);
+ConfigEntry* config_pushHex(const int8_t* variable,const uint32_t value);
 ConfigEntry* config_find(const int8_t* variable);
+void config_remove(const int8_t* variable);
 ConfigEntry* config_replaceS(const int8_t* variable,const int8_t* newValue);
 ConfigEntry* config_replace(ConfigEntry* e,const int8_t* newValue);
-
 int8_t* config_getS(const int8_t* variable);
+int8_t* config_getHex(const int8_t* variable);
 int32_t config_getI(const int8_t* variable);
-
+float config_getF(const int8_t* variable);
+double config_getD(const int8_t* variable);
 int32_t config_loadFromBuffer(const int8_t* buf,const int32_t size);
 int32_t config_saveToBuffer(int8_t* buf);
 int32_t config_getEntryCount();
-
+uint32_t config_predictOutputBufferSize();
 #endif
 
