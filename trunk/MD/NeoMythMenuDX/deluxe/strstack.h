a408f032d35e73b675fa485b22f9857c2755ff57
conleon1988 <conleon1988@e68fe1e2-52bc-dc62-6954-b9b5b324de01>
2010-09-17 20:20:40 +0000

    MD string stack..

---
+++ trunk/MD/NeoMythMenuDX/deluxe/strstack.h
@@ -0,0 +1,28 @@
+#ifndef __strstack_h__
+#define __strstack_h__
+
+#define STRINGSTACK_BLOCK_SIZE (1024)
+#define STRINGSTACK_BLOCK_DIVISIONS (4)
+
+extern unsigned char strstackBlock[];
+extern unsigned short strstackBlockPtr;
+
+void strstack_init();
+
+#define strstack_push(TYPE)\
+((strstackBlockPtr > STRINGSTACK_BLOCK_DIVISIONS-1) ? (TYPE*)strstackBlock[STRINGSTACK_BLOCK_DIVISIONS-1)] : (TYPE*)strstackBlock[strstackBlockPtr++])
+
+#define strstack_pop(TYPE)\
+((strstackBlockPtr <= 0) ? (TYPE*)strstackBlock[0] : (TYPE*)strstackBlock[--strstackBlockPtr])
+
+#define cstrstack_push() strstack_push(char)
+#define ustrstack_push() strstack_push(unsigned char)
+#define wstrstack_push() strstack_push(WCHAR)
+#define xstrstack_push() strstack_push(XCHAR)
+#define cstrstack_pop() strstack_pop(char)
+#define ustrstack_pop() strstack_pop(unsigned char)
+#define wstrstack_pop() strstack_pop(WCHAR)
+#define xstrstack_pop() strstack_pop(XCHAR)
+
+#endif
+
