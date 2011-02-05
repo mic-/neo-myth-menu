69fb388b99e9269b372c184ac80fd89f16e8314f
ChillyWillyGuru <ChillyWillyGuru@e68fe1e2-52bc-dc62-6954-b9b5b324de01>
2011-02-05 00:43:52 +0000

    clean up plus extra io with comments interrupt handling added, can now use vblank and pause

--- SMS/NeoMythMenu/vdp.c
+++ SMS/NeoMythMenu/vdp.c
@@ -3,32 +3,38 @@
 
 void vdp_set_reg(BYTE rn, BYTE val)
 {
-	VdpCtrl = val;
-	VdpCtrl = rn | CMD_VDP_REG_WRITE;
+    disable_ints;
+    VdpCtrl = val;
+    VdpCtrl = rn | CMD_VDP_REG_WRITE;
+    enable_ints;
 }
 
 
 void vdp_set_vram_addr(WORD addr)
 {
-	VdpCtrl = (addr & 0xFF);
-	VdpCtrl = (addr >> 8) | CMD_VRAM_WRITE;
+    disable_ints;
+    VdpCtrl = (addr & 0xFF);
+    VdpCtrl = (addr >> 8) | CMD_VRAM_WRITE;
+    enable_ints;
 }
 
 
 void vdp_set_cram_addr(WORD addr)
 {
-	VdpCtrl = (addr & 0xFF);
-	VdpCtrl = (addr >> 8) | CMD_CRAM_WRITE;
+    disable_ints;
+    VdpCtrl = (addr & 0xFF);
+    VdpCtrl = (addr >> 8) | CMD_CRAM_WRITE;
+    enable_ints;
 }
 
 
 void vdp_copy_to_vram(WORD dest, BYTE *src, WORD len)
 {
-	vdp_set_vram_addr(dest);
+    vdp_set_vram_addr(dest);
 
-	while (len--)
-	{
-		VdpData = *src++;
-	}
+    while (len--)
+    {
+        VdpData = *src++;
+    }
 }
 
