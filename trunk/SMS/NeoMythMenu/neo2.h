eae59af4f792567f4e44b9d37cecf5706b0497cd
ChillyWillyGuru <ChillyWillyGuru@e68fe1e2-52bc-dc62-6954-b9b5b324de01>
2011-02-13 07:38:22 +0000

    added routines to read/write the psram

--- trunk/SMS/NeoMythMenu/neo2.h
+++ trunk/SMS/NeoMythMenu/neo2.h
@@ -68,6 +68,8 @@ extern void neo2_asic_end();
 
 extern void neo2_ram_to_sram(BYTE dsthi, WORD dstlo, BYTE* src, WORD len);
 extern void neo2_sram_to_ram(BYTE* dst, BYTE srchi, WORD srclo, WORD len);
+extern void neo2_ram_to_psram(BYTE dsthi, WORD dstlo, BYTE* src, WORD len);
+extern void neo2_psram_to_ram(BYTE* dst, BYTE srchi, WORD srclo, WORD len);
 
 extern void neo2_run_game_gbac();
 
